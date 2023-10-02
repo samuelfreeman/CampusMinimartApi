a7c802ee9471e9bbec75e3899d81de39421b06ef
samuelfreeman <samuelmanueljnr@gmail.com>
2023-10-02 15:44:29 +0000

    new update

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 require('dotenv').config();
-
 const bodyParser = require('body-parser');
 const apiRoute = require('./routes/index');
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 const port = process.env.PORT;
+
+app.use(cors({ origin: true, credentials: true }));
 
 app.get('/', function (req, res) {
   res.send('Hello chippyCode');
